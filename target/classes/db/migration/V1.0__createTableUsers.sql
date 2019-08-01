5cbbce26bbdac654efc688369c4e2eeb0503b5fb
Dimitra Zafeiropoulou <dzafeiropoulou@bol.com>
2019-08-01 13:49:44 +0200

    delete accidentally pushed targe

--- target/classes/db/migration/V1.0__createTableUsers.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users
-  (
-     id         INT NOT NULL,
-     username   VARCHAR(100) UNIQUE NOT NULL,
-     password   VARCHAR (100),
-     email      VARCHAR (250),
-     PRIMARY KEY (id)
-  );
